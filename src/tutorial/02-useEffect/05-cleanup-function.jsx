1b2be015347c5e86402aa2d1ef3f3d6d4c08591d
Gimba Manasseh A <manasseh.gimba.a@gmail.com>
2023-05-05 11:25:46 +0100

    cleanup functions

--- src/tutorial/02-useEffect/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/05-cleanup-function.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  // console.log(toggle);
+  return (
+    <div>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        toggle comp
+      </button>
+      {toggle && <RandomComp />}
+    </div>
+  );
 };
 
+const RandomComp = () => {
+  // when we use useEffect on a component being rendered conditionally it will run not just once
+  // but as many times as that condition is true or met regardless of whether the dependency array for the useEffect is empty
+  // useEffect(() => {
+  //   const intId = setInterval(() => {
+  //     console.log("hello from interval");
+  //   }, 1000);
+  //   return () => {
+  //     clearInterval(intId);
+  //     console.log("cleanup");
+  //   };
+  //   console.log(
+  //     "hmmmm, interesting mount and unmount initiating re-render everytime"
+  //   );
+  // }, []);
+  useEffect(() => {
+    const someFunc = () => {
+      // some logic
+    };
+    window.addEventListener("scroll", someFunc);
+    return () => window.removeEventListener("scroll", someFunc);
+  }, []);
+  return <h1>Hello there....</h1>;
+};
 export default CleanupFunction;
--- src/tutorial/03-conditional-rendering/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/07-user-challenge.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+  const login = () => {
+    // normally this information is gotten from the DB or API
+    setUser({ name: "Vegan Food Truck" });
+  };
+  const logout = () => {
+    setUser(null);
+  };
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>Hello there, {user.name}</h4>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please Login</h4>
+          <button className="btn" onClick={login}>
+            Login
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
