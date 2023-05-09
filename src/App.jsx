a5767365c7c5b0e6cfe1d56bdffb24cd3ddb4614
Gimba Manasseh A <manasseh.gimba.a@gmail.com>
2023-05-09 17:38:40 +0100

    using javascript to address certain errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import Starter from "./tutorial/02-useEffect/05-cleanup-function";
+import List from "../src/tutorial/05-leverage-javascript/starter/List";
 function App() {
   return (
     <div className="container">
-      <Starter />
+      <h3>Leverage Javascript</h3>
+      <List />
     </div>
   );
 }
---
+++ src/tutorial/04-project-structure/starter/Navbar/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <h2>Navbar folder structure</h2>;
+};
+
+export default Navbar;
